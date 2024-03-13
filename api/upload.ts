9cf526a46a320920beea1ac3fc513e8ceb741452
aintira11 <aintrlala@gmail.com>
2024-03-13 23:42:42 +0700

    firebase

--- api/upload.ts
+++ api/upload.ts
@@ -8,32 +8,44 @@ import { ModelPhoto } from "../model/model";
 export const router = express.Router();
 
 //เชื่อม firebase
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDEg4DwZhyqF-xPHxQx2w0DdR6l0UdbtNs",
+  authDomain: "vote-projectadv.firebaseapp.com",
+  projectId: "vote-projectadv",
+  storageBucket: "vote-projectadv.appspot.com",
+  messagingSenderId: "197613618112",
+  appId: "1:197613618112:web:9fb64dc47b6c5876b37e25",
+  measurementId: "G-2STHK93H34"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+const storage = getAnalytics();
+
 
 class FileMiddleware {
   filename = "";
   public readonly diskLoader = multer({
-    storage: multer.diskStorage({
-      destination: (_req, _file, cb) => {
-        cb(null, path.join(__dirname, "../uploads"));
-      },
-      filename: (req, file, cb) => {
-        const uniqueSuffix =
-          Date.now() + "-" + Math.round(Math.random() * 10000);
-        this.filename = uniqueSuffix + "." + file.originalname.split(".").pop();
-        cb(null, this.filename);
-      },
-    }),
-    limits: {
-      fileSize: 67108864, // 64 MByte
-    },
-  });
+    //
+  storage: multer.memoryStorage(),
+  limits: {
+    fileSize: 67108864, // 64 MByte
+  },
+});
 }
 
 const fileUpload = new FileMiddleware(); 
 //อัพรูป
 router.post("/", fileUpload.diskLoader.single("Photo"), async (req, res) => {
   
-  // const Photo = "https://apivote-project.onrender.com/uploads/"+fileUpload.filename;
   const Photo ="https://apivote-project.onrender.com/uploads/"+fileUpload.filename;
   let UserID : ModelPhoto =req.body;
   const currenData = new Date().toISOString();
